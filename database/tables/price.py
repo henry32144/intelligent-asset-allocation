f79e9c6260c59d6062bc1bb0de7163f81352db8d
Sam Hsia <sam0213442dif@gmail.com>
2020-07-15 15:21:31 +0800

    add the function to save stock price

--- database/tables/price.py
+++ database/tables/price.py
@@ -1,4 +1,5 @@
-from main import db
+from database.database import db
+import pandas as pd
 
 class StockPrice(db.Model):
     __tablename__ = 'price'
@@ -11,5 +12,26 @@ class StockPrice(db.Model):
     close = db.Column(db.Float(precision=4))
     vol = db.Column(db.Integer)
     adj_close = db.Column(db.Float(precision=4))
+    query = db.Column(db.String(20))
 
 
+    def __init__(self, date, high, low, open_, close, vol, adj_close, query):
+        self.date = date
+        self.high = high
+        self.low = low
+        self.open = open_
+        self.close = close
+        self.vol = vol
+        self.adj_close = adj_close
+        self.query = query
+
+    def __repr__(self):
+        return "Stock Price ('{}', '{}', '{}', '{}')".format(self.date, self.vol, self.adj_close, self.query)
+
+
+def save_to_db(filename):
+	df = pd.read_csv(filename)
+	_lst = []
+	for index, row in df.iterrows():
+		_lst.append(StockPrice(row['Date'], row['High'], row['Low'], row['Open'], row['Close'], row['Volume'], row['Adj Close'], row['query']))
+	db.session.add_all(_lst)
