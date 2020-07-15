0d5acc4ab49ed2f76dfbb530944ea0720c65eba1
Sam Hsia <sam0213442dif@gmail.com>
2020-07-15 15:22:37 +0800

    add the stock price function

--- test_cases.py
+++ test_cases.py
@@ -5,27 +5,25 @@ from flask import Blueprint, abort, request, render_template
 from database.tables.user import User
 from database.database import db
 from database.tables.crawling_data import CrawlingData, read_csv
+from database.tables.price import StockPrice, save_stock_price_to_db
+
 test_cases = Blueprint('test_cases', __name__)
 
 @test_cases.route('/query_user_test')
 def query():
     result = User.query.all()
     response = 'Not found'
-    if len(result) > 0:
-        response = result[0].user_name
+    if result != None:
+        response = result[0].username
     return response
 
 @test_cases.route('/create_user_test')
 def create_user_test():
-    result = User.query.filter_by(user_name='test').first()
+    result = User.query.filter_by(username='test').first()
     print(result)
     if result is None:
         # create test row
-        test = User(
-            user_name='Test User',
-            user_email='test@example.com',
-            user_password='123'
-        )
+        test = User(username='Test User', email='test@example.com')
         db.session.add(test)
         db.session.commit()
         print("create new user")
@@ -43,4 +41,10 @@ def crawling_test():
 @test_cases.route('/create_crawling_test')
 def create_crawling_test():
     read_csv('./database/tables/test.csv')
+    return ''
+
+
+@test_cases.route('/create_pricetable_test')
+def create_pricetable_test():
+    save_stock_price_to_db('./database/tables/test_stock_price.csv')
     return ''
