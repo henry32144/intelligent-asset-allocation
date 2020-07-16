c75105dc711bcedee6235c8ec81aff952b73ae92
Yang Wang <penguinwang@smail.nchu.edu.tw>
2020-07-16 14:43:43 +0800

    update top k

--- notebook/config.py
+++ notebook/config.py
@@ -1,16 +1,19 @@
 import torch
 import numpy as np
 from transformers import AutoTokenizer
+RANDOM_SEED = 2020
+np.random.seed(RANDOM_SEED)
+torch.manual_seed(RANDOM_SEED)
 
 
+# Data configuration
+TOP_K = 3
+
+# Model configuration
 BATCH_SIZE = 8
-MAX_LEN = 64
+MAX_LEN = 32
 EPOCHS = 4
 PRE_TRAINED_MODEL_NAME = 'distilbert-base-uncased'
 tokenizer = AutoTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 MODEL_PATH = "./weights/distilbert.bin"
-
-RANDOM_SEED = 2020
-np.random.seed(RANDOM_SEED)
-torch.manual_seed(RANDOM_SEED)
