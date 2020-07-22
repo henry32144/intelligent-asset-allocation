9584b0b5f1962c653e8f6bde07f1a47acc7b7aeb
Yang Wang <penguinwang@smail.nchu.edu.tw>
2020-07-22 10:08:43 +0800

    update model

--- model/train.py
+++ model/train.py
@@ -1,3 +1,4 @@
+import sys
 import warnings
 import logging
 import config
@@ -71,6 +72,33 @@ def matthews_correlation_coefficient(true_pos, true_neg, false_pos, false_neg):
     denominator = np.sqrt((true_pos+false_pos)*(true_pos+false_neg)*(true_neg+false_pos)*(true_neg+false_neg)) + 1e-7
     return (nominator / denominator)
 
+class ProgressBar(object):
+    """Cheers @ajratner"""
+
+    def __init__(self, n, length=40):
+        # Protect against division by zero
+        self.n      = max(1, n)
+        self.nf     = float(n)
+        self.length = length
+        # Precalculate the i values that should trigger a write operation
+        self.ticks = set([round(i/100.0 * n) for i in range(101)])
+        self.ticks.add(n-1)
+        self.bar(0)
+
+    def bar(self, i, message=""):
+        """Assumes i ranges through [0, n-1]"""
+        if i in self.ticks:
+            b = int(np.ceil(((i+1) / self.nf) * self.length))
+            sys.stdout.write("\r[{0}{1}] {2}%\t{3}".format(
+                "="*b, " "*(self.length-b), int(100*((i+1) / self.nf)), message
+            ))
+            sys.stdout.flush()
+
+    def close(self, message=""):
+        # Move the bar to 100% before closing
+        self.bar(self.n-1)
+        sys.stdout.write("{0}\n\n".format(message))
+        sys.stdout.flush()
 
 def train_distilbert(model, data_loader, loss_function, optimizer, device, scheduler, n_examples):
     model = model.train()
