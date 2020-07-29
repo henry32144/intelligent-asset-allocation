05ee4a190c1fba8731ac1ee16bba895e71069b57
Yang Wang <penguinwang@smail.nchu.edu.tw>
2020-07-29 16:11:52 +0800

    update

--- model/deep_stock.py
+++ model/deep_stock.py
@@ -889,10 +889,10 @@ def test_distilbert(model, data_loader, device):
 
 def main():
     print("Loading data...")
-    train_data = joblib.load("./data/train_top10_v1.bin")
-    train_data = train_data[train_data["sector"] == "Financials"]
-    valid_data = joblib.load("./data/valid_top10_v1.bin")
-    valid_data = valid_data[valid_data["sector"] == "Financials"]
+    train_data = joblib.load("./data/train_top10_v2.bin")
+    # train_data = train_data[train_data["sector"] == "Financials"]
+    valid_data = joblib.load("./data/valid_top10_v2.bin")
+    # valid_data = valid_data[valid_data["sector"] == "Financials"]
     print("Done!")
 
     print("Loading model...")
