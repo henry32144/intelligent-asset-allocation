8eda93e616a9b3015f8f71c94922f80e821c5d8e
Dennis Wu <boriscat999@gmail.com>
2020-08-04 16:16:44 +0800

    1111

--- model/get_portfolio.py
+++ model/get_portfolio.py
@@ -3,9 +3,9 @@ from database.tables.user import User, get_companys
 from database.tables.company import Company
 from database.tables.portfolio import Portfolio
 
-def return_portfolio(mode = 'basic', user_id, portfolio_id ):
+def return_portfolio(mode = 'basic', portfolio_id ):
 	if (mode == 'basic'):
-		data = Portfolio.query.filter(and_(user_id == user_id, portfolio_name == portfolio_id)  )
+		data = Portfolio.query.filter(Portfolio.portfolio_name == portfolio_id)
 		companys = get_companys(data.portfolio_stocks)
 		marko = Markowitz(companys)
 	    all_weights = marko.get_all_weights()
@@ -15,3 +15,6 @@ def return_portfolio(mode = 'basic', user_id, portfolio_id ):
 	    print('all_values:', all_values)
 	    print('all_return:', all_return)
 	    return all_weights, all_values, all_return
+
+	elif(mode == 'blacklitterman'):
+		pass
