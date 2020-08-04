60326d1018ff3d6bd50b4fe7beb844a4de0f99b3
penguinwang96825 <penguinwang@smail.nchu.edu.tw>
2020-08-04 08:55:43 +0800

    update

--- model/zero_shot_learner.py
+++ model/zero_shot_learner.py
@@ -75,12 +75,12 @@ def extend_df_with_cos_sim(df, col, labels, sort_by):
     """
     SB = SentenceBert()
     print("Start zero-shot learner...")
-    df[labels] = df.apply(lambda row: pd.Series(SB.get_similarity(row[col], labels=labels)), axis=1)
-    # for index, row in df.iterrows():
-    #     sim_dict = SB.get_similarity(row[col], labels)
-    #     for i in range(len(labels)):
-    #         df.loc[index, labels[i]] = sim_dict[labels[i]]
-    # df = df.sort_values(by=sort_by, axis=0, ascending=False)
+    # df[labels] = df.apply(lambda row: pd.Series(SB.get_similarity(row[col], labels=labels)), axis=1)
+    for index, row in df.iterrows():
+        sim_dict = SB.get_similarity(row[col], labels)
+        for i in range(len(labels)):
+            df.loc[index, labels[i]] = sim_dict[labels[i]]
+    df = df.sort_values(by=sort_by, axis=0, ascending=False)
     df = df.reset_index(drop=True)
     print("Done!")
     return df
