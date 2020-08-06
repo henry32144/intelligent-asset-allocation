85b2856a21234abd6ca7a8531bfe42e96b2e4231
Yang Wang <penguinwang@smail.nchu.edu.tw>
2020-08-06 23:34:44 +0800

    update all file

--- model/cool.py
+++ model/cool.py
@@ -14,10 +14,8 @@ import pandas as pd
 import numpy as np
 import pysentiment as ps
 from tqdm import tqdm
-
-def cat(sents):
+def cat( sents ):
 	lm = ps.LM()
-
 	sents = sent_tokenize(sents)
 	res = {}
 	for s in sents:
@@ -28,40 +26,44 @@ def cat(sents):
 		else:
 			res[s] = 0
 	res = sorted(res.items(), key=lambda x: x[1])  
-	res = [x[0] for x in res ]
+	res = [ x[0] for x in res  ]
 	return res
 
-df = joblib.load('sp500_top100_v4.bin')
+# df = joblib.load('sp500_top100_v4.bin')
+# df = df.drop_duplicates(subset = 'content')
 
-
-def main(data):
+def transform(data):
+	qq = 0
 	k = 25
 	new_cols = ["Top {} News".format(i + 1) for i in range(k)]
+	new_cols.insert(0, 'date')
 	new_cols.insert(0, 'ticker')
-	result_df = pd.DataFrame(columns=new_cols)
+	result_df = pd.DataFrame(columns = new_cols)
 	for ticker in data['ticker'].unique().tolist():
-		print(ticker)
-		qq += 1
-		new_df = pd.DataFrame(columns=new_cols)
+		new_df = pd.DataFrame(columns = new_cols)
 		df = data[data['ticker'] == ticker]
-		for i, row in tqdm(df.iterrows(), total=len(df), desc=ticker+'get sent'):
-			content = cat(row['content'])
-			if(len(content) < 25):
-				for jj in range(25-len(content)):
+		for dt in tqdm(df['date'].unique().tolist(), total = len(df['date'].unique().tolist())):
+			context = ""
+			df_date = df[df['date'] == dt]
+			for i0, row0 in df_date.iterrows():
+				context += row0['content'] 
+			# for i, row in tqdm( df.iterrows(), total = len(df) , desc = ticker + 'get sent'):
+			content = cat(context)
+			if( len(content) < 25 ):
+				for jj in range(25 - len(content)):
 					content.append(np.NaN)
 			else:
 				content = content[:25]
-			# print(content)
-			content.insert(0, ticker)
+			content.insert(0, dt)
+			content.insert(0,ticker)
 			content = [tuple(content)]
-			new_df = pd.DataFrame(content, columns=new_cols)
-			result_df = pd.concat([new_df, result_df], ignore_index=True)
-	joblib.dump(result_df, 'result.bin', compress=5)
-
-main(df)
-
-def test():
-	a = joblib.load('result.bin')
-	print(a)
-
-test()
+			new_df = pd.DataFrame( content, columns = new_cols )
+			result_df = pd.concat([ new_df, result_df ],ignore_index=True)
+	return result_df
+			# joblib.dump(result_df , 'result.bin',compress=5)
+# main(df)
+# joblib.dump(main(df) , 'result.bin',compress=5)
+# def test():
+# 	a = joblib.load('result.bin')
+# 	print(a)
+# test()
