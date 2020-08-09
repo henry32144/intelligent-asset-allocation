fe1567eda5d9c60fb091aae2eda8aa2cb1999f9a
ChengHan <henry32144@hotmail.com>
2020-08-09 19:37:20 +0800

    feat: finish layout of landing page

---
+++ static/precache-manifest.e3e1b65eb78a874079feba0c7999c3ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0232696af95c4efa063a3649c9ca22f",
+    "url": "../index.html"
+  },
+  {
+    "revision": "8e033195ac8752e60a5b",
+    "url": "../static/css/main.67628981.chunk.css"
+  },
+  {
+    "revision": "38f7a8ff7de3e02741b6",
+    "url": "../static/js/2.01c6286c.chunk.js"
+  },
+  {
+    "revision": "41f20da7e3dcfa05585e7fa7cea1e0d4",
+    "url": "../static/js/2.01c6286c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e033195ac8752e60a5b",
+    "url": "../static/js/main.3b364067.chunk.js"
+  },
+  {
+    "revision": "69a04df14c07aea88571",
+    "url": "../static/js/runtime-main.720e0a57.js"
+  }
+]);
